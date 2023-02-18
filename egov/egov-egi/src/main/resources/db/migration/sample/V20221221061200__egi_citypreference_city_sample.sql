00122e964f979a29529befdc68bd6b5f2cf204e2
spiliti <beenzu7@gmail.com>
2023-02-18 05:27:12 +0200

    Updated  preferences in city

--- egov/egov-egi/src/main/resources/db/migration/sample/V20221221061200__egi_citypreference_city_sample.sql
+++ egov/egov-egi/src/main/resources/db/migration/sample/V20221221061200__egi_citypreference_city_sample.sql
@@ -4,6 +4,6 @@ update eg_city set domainurl = '41.63.0.133', name ='Mapepe City Council',  loca
 
 insert into eg_citypreferences (id, municipalitylogo, createdby,createddate, lastmodifiedby, lastmodifieddate, version, municipalityname) values (nextval('seq_eg_citypreferences'),null,1, now(),1,now(), 0, 'Mapepe City Council');
 
-update eg_city set preferences = 1; --(select cp.id from eg_city c, eg_citypreferences cp where municipalityname = 'Mapepe City Council' and c.name = cp.municipalityname);
+update eg_city set preferences = 1; 
 
 update eg_citypreferences set recaptchapk = '6LfidggTAAAAANDSoCgfkNdvYm3Ugnl9HC8_68o0', recaptchapub = '6LfidggTAAAAADwfl4uOq1CSLhCkH8OE7QFinbVs';
